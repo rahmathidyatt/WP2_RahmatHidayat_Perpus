63631c71f9c7e557b986b727e2bf5ff7b0dd403b
Rahmat Hidayat <90608676+rahmathidyatt@users.noreply.github.com>
2021-11-30 18:24:28 +0700

    Pertemuan 10 Membuat Form Registrasi User

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -31,5 +31,5 @@
                       </div>
                       <!-- /.container-fluid -->
                     </div>
-                    <!-- End of Main Content -->
+                    <!-- End of Main Content --> 
                     
